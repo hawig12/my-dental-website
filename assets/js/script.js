3a4a413229808fb88bd13c21ff0caa44d105ca78
hawig12 <nabiee1264@gmail.com>
2025-10-15 23:32:28 +0300

    Add files via upload

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,57 @@
+'use strict';
 
+
+/**
+ * addEvent on element
+ */
+
+const addEventOnElem = function(elem, type, callback){
+    if (elem.length > 1){
+        for (let i=0; i < elem.length; i++){
+            elem[i].addEventListener(type, callback);
+        }
+    }
+        else{
+            elem.addEventListener(type, callback);
+        }
+    }
+
+
+
+/**
+ * navbar toggle
+ */
+
+const navbar = document.querySelector("[data-navbar]");
+const navbarLinks = document.querySelectorAll("[data-nav-link]");
+const navbarToggler = document.querySelector("[data-nav-toggler]")
+
+const togglerNav = function () {
+    navbar.classList.toggle("active");
+    navbarToggler.classList.toggle("active");
+}
+
+addEventOnElem(navbarToggler, "click", togglerNav);
+
+const closeNav = function () {
+    navbar.classList.remove("active");
+    navbarToggler.classList.remove("active");
+}
+
+addEventOnElem(navbarLinks, "click", closeNav);
+
+/**
+ * header active
+ */
+const header = document.querySelector("[data-header]");
+const backTopBtn = document.querySelector("[data-back-top-btn]");
+
+window.addEventListener ("scroll", function () {
+    if(this.window.scrollY >= 100){
+        header.classList.add("active");
+        backTopBtn.classList.add("active");
+    } else{
+        header.classList.remove("active");
+        backTopBtn.classList.remove("active")
+    }
+});
